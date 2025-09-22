68021c5178029c694ab095f8a09c127ee96b8d94
A.Hegarty <a.hegarty004@gmail.com>
2025-09-22 17:39:43 +0100

    added comments - AH

--- sample chess/chess_Code.js
+++ sample chess/chess_Code.js
@@ -1,17 +1,20 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const square_Size = 50; 
-//setting the square size to 50 locks keeps the board at 8*8 squares,
+const square_Size = 75; 
+//setting the square size to 75 keeps the board at 8*8 squares,
 //standard chessboard size
 
-//fucntion prints a checkerboard proportional to the dimensions of the canvas element
+//fucntion prints a checkerboard using the dimensions of the canvas element and square_Size const
 function checkerboard() {
+    //for loops make the grid for the board, drawing squares
     for (let row = 0; row < canvas.height / square_Size; row++){
         for (let collumn = 0; collumn < canvas.width / square_Size; collumn++){
             const x = collumn * square_Size;
             const y = row * square_Size;
 
+            //if statement lets the squares be different colours
+            //beige and brown chosen so black and white pieces can be clearly seen on both kinds of squares
             if((row + collumn) % 2 === 0){
                 ctx.fillStyle = "beige";
             } else {
