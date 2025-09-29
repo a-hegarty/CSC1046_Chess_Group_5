5b3132cc1047248877c7b29682217b9d64ee60c9
A.Hegarty <a.hegarty004@gmail.com>
2025-09-29 15:09:03 +0100

    changed colour of black squaredfrom brown to grey -AH

--- MyChess/chess_Code.js
+++ MyChess/chess_Code.js
@@ -3,8 +3,7 @@ const ctx = canvas.getContext("2d");
 
 const square_Size = 75; 
 /*setting the square size to 75 keeps the board at 8*8 squares,
-standard chessboard size,
-on a 600*600 canvas*/
+standard chessboard size, on a 600*600 canvas*/
 
 //fucntion prints a checkerboard using the dimensions of the canvas element and square_Size const
 function checkerboard() {
@@ -19,7 +18,7 @@ function checkerboard() {
             if((row + collumn) % 2 === 0){
                 ctx.fillStyle = "beige";
             } else {
-                ctx.fillStyle = "brown";
+                ctx.fillStyle = "grey";
             }
 
             ctx.fillRect(x, y, square_Size, square_Size);
