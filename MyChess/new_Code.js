271d87777b557b9b0525b28ca95e25a2f57e30dc
A.Hegarty <a.hegarty004@gmail.com>
2025-11-10 09:44:00 +0000

    added forfeit function to new_Code

--- MyChess/new_Code.js
+++ MyChess/new_Code.js
@@ -9,12 +9,42 @@ function insertImage(){
             image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.png">`;
             image.style.cursor = 'pointer';
         }
-    }
-    )
+    })
+}
+
+function colour_Squares(){
+    const colour = document.querySelectorAll('.square');
+
+    colour.forEach(colour => {
+        getId = colour.id;
+        arr = Array.from(getId);
+        arr.shift();
+        aside = eval(arr.pop());
+        aup = eval(arr.shift());
+        a = aside + aup;
+    })
+}
+
+function forfeit(){
+   const forfeit_Button = document.getElementById("forfeit");
+    
+   //on click...
+    forfeit_Button.addEventListener("click", function(){
+        //...confirn decision...
+        let confirm_forfeit = confirm("Are you sure you would like to forfeit this game?");
+        if(confirm_forfeit == true){
+            // if player wants to forfeit, the game ends
+            alert("You have forfeited the game.");
+        } else {
+            //otherwise play continues as normal
+            alert("You have not forfeited the game\nReturning to game");
+        }
+    }); 
 }
 
 function init(){
-    insertImage()
+    insertImage();
+    forfeit();
 }
 
 init();
